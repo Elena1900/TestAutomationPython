b74c739c62aad8c03de5b8e733fe868ba625fa5b
Elena1900 <oliverres@mail.ru>
2021-01-05 12:39:55 +0100

    Улучшаем тест

--- tests/test_items.py
+++ tests/test_items.py
@@ -2,4 +2,6 @@ def test_busket_button_present(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.implicitly_wait(10)
     browser.get(link)
-    assert browser.find_element_by_xpath('//form[@id="add_to_basket_form"]/button[@type = "submit"]')
+    button = browser.find_elements_by_css_selector("#add_to_basket_form button[type='submit']")
+
+    assert len(button) > 0, "Корзина не найдена!"
